cfe110544c18586b5a63b032ec44fe17869038b8
zdh2019 <429305334@qq.com>
2020-03-16 15:59:55 +0800

    图片上传

--- src/main/java/zhangdahu/community/controller/FileController.java
+++ src/main/java/zhangdahu/community/controller/FileController.java
@@ -2,20 +2,38 @@ package zhangdahu.community.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import zhangdahu.community.dto.FileDto;
+import zhangdahu.community.provider.UCloudProvider;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Controller
 public class FileController {
+    @Autowired
+    private UCloudProvider uCloudProvider;
+
     @RequestMapping("/file/upload")
     @ResponseBody
-    public FileDto upload()
+    public FileDto upload(HttpServletRequest request)
     {
-        FileDto dto=new FileDto();
-        dto.setSuccess(1);
-        dto.setUrl("/img/huaji.jpg");
-        return dto;
+        MultipartHttpServletRequest multipartHttpServletRequest=(MultipartHttpServletRequest) request;
+        MultipartFile file = multipartHttpServletRequest.getFile("editormd-image-file");
+        try {
+            String fileName = uCloudProvider.upload(file.getInputStream(), file.getContentType(), file.getOriginalFilename());
+            FileDto fileDTO = new FileDto();
+            fileDTO.setSuccess(1);
+            fileDTO.setUrl(fileName);
+            return fileDTO;
+        } catch (Exception e) {
+            FileDto fileDTO = new FileDto();
+            fileDTO.setSuccess(0);
+            fileDTO.setMessage("上传失败");
+            return fileDTO;
+        }
     }
 }
--- src/main/java/zhangdahu/community/exception/CustomizeErrorCode.java
+++ src/main/java/zhangdahu/community/exception/CustomizeErrorCode.java
@@ -9,7 +9,10 @@ public enum  CustomizeErrorCode implements ICustomizeErrorCode {
     COMMENT_NOT_FOUND(2006,"评论未找到,换个评论试试"),
     COMMENT_IS_EMPTY(2007,"输入内容不能为空"),
     NOTIFICATION_NO_FOUND(2008,"评论不存在"),
-    READ_NOTIFICATION_FAIL(2009,"读取评论出错，请重试");
+    READ_NOTIFICATION_FAIL(2009,"读取评论出错，请重试"),
+    FILE_UPLOAD_FAIL(2010, "图片上传失败"),
+    INVALID_INPUT(2011, "非法输入"),
+    INVALID_OPERATION(2012, "兄弟，是不是走错房间了？");
 
     @Override
     public String  getMessage()
